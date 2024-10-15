52e58ef063a267ee99403c271a2bf9b679b6947b
Chingkheinganba Thoudam <72967745+chingkhei-th@users.noreply.github.com>
2024-10-15 14:36:40 +0530

    Add gitsigns.nvim plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,11 @@
 return {
-    {
-        'vhyrro/luarocks.nvim',
-        priority = 1001, -- this plugin needs to run before anything else
-        opts = {
-        rocks = { 'magick' },
-        },
-    },
+    -- {
+    --     'vhyrro/luarocks.nvim',
+    --     priority = 1001, -- this plugin needs to run before anything else
+    --     opts = {
+    --     rocks = { 'magick' },
+    --     },
+    -- },
 
     {
         "nvim-treesitter/nvim-treesitter",
